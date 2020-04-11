3026ec8eb1805d4317fac25143792af8b064ab7b
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-11 11:53:11 +0530

    [Shivam] Function to open the address book file specified Returns the addressbook or None.

---
+++ address-book.py
@@ -0,0 +1,61 @@
+import json, os
+
+class addressbook:
+
+    def __init__(self, fname, lname, address, city, state, zipCode, phone):
+        self.fname = fname
+        self.lname = lname
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipCode = zipCode
+        self.phone = phone 
+
+    def __str__(self):
+        return "First Name:{0}\nLast Name:{1}\nAddress:{2}\nCity:{3}\nstate:{4}\nZip Code:{5}\nPhone No:{6}".format(self.fname,self.lname,self.address,self.city,self.state,self.zipCode,self.phone)
+
+def open_addressbook(filepath):
+    """
+    Function to open the address book file specified
+    Returns the addressbook or None
+    """
+    path_exists = os.path.exists(filepath)
+    addressbook = None
+
+    if path_exists:
+        with open(filepath, 'r') as infile:
+            addressbook = json.load(infile)
+    
+    return addressbook
+
+def menu():
+    '''
+    Menu of programs
+    '''
+    print('''
+    1.Open Address book(load json file)
+    ''')
+
+def switchToFunction(case,filepath):
+    '''
+    Create switch function to move perticular program
+    '''
+    switcher = {
+        1 : lambda: open_addressbook(filepath),
+        }
+    func = switcher.get(case, lambda: 'Invalid choice please select correct options.')
+    print(func())
+
+def main():
+    menu()
+    choice = int(input("Enter which program you want to run: "))
+    FILE_PATH = "data/address_book_file.json"
+    switchToFunction(choice,FILE_PATH)
+    options = input('Do you want to continue?[y/n]: ')
+    if options.lower() == 'y':
+        main()
+    else:
+        exit()
+
+if __name__ == "__main__":
+    main()
